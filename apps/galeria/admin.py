4f2a9a59ce8651b5ca7b3d2f15299dcf241d6a61
sandroeg <sandroeg@gmail.com>
2024-01-28 17:58:32 -0300

    Fim do curso de introdução ao Django da Alura

---
+++ apps/galeria/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from apps.galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display= ("id","nome","legenda", "publicada")
+    list_display_links=("id", "nome")
+    search_fields= ("nome",)
+    list_filter=("categoria","usuario")
+    list_per_page=10
+    list_editable=("publicada",)
+admin.site.register(Fotografia, ListandoFotografias)
